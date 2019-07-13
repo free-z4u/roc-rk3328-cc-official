34929335b0e5d6247c2b62a0a208f140de1b0225
Denis Pauk <pauk.denis@gmail.com>
2019-07-13 22:32:14 +0300

    Remove untested ROCKCHIP_PVTM

--- include/soc/rockchip/rockchip_opp_select.h
+++ include/soc/rockchip/rockchip_opp_select.h
@@ -8,8 +8,5 @@
 
 int rockchip_of_get_lkg_scale_sel(struct device *dev, char *name);
 int rockchip_of_get_lkg_volt_sel(struct device *dev, char *name);
-int rockchip_of_get_pvtm_volt_sel(struct device *dev,
-				  char *clk_name,
-				  char *reg_name);
 
 #endif
